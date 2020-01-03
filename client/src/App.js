7f57cdc6739fa0b0268de93de8aaf431003da206
Sergei Petrov <sergey.otis80@gmail.com>
2020-01-03 20:44:19 +0300

    Alert actions and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
@@ -7,12 +9,14 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Landing />
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Landing />
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
