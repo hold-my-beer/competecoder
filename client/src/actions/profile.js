204ce295088fbbd4949c319b69a2295986ae06ba
Sergei Petrov <sergey.otis80@gmail.com>
2020-01-13 22:09:01 +0300

    Starting with Profile component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -43,6 +43,23 @@ export const getProfiles = () => async dispatch => {
   }
 };
 
+//Get profile by ID
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/profile/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
 //Create profile / Update profile
 export const createProfile = (
   formData,
